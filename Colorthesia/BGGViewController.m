1c05ae8be72b00e6a861d10ca41e4b8599bcf7f5
AJ Green <ajgreen1987@gmail.com>
2014-09-30 20:22:09 -0400

    Initial Commit

---
+++ Colorthesia/BGGViewController.m
@@ -0,0 +1,29 @@
+//
+//  BGGViewController.m
+//  Colorthesia
+//
+//  Created by AJ Green on 9/30/14.
+//  Copyright (c) 2014 Big Gorilla Games. All rights reserved.
+//
+
+#import "BGGViewController.h"
+
+@interface BGGViewController ()
+
+@end
+
+@implementation BGGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
